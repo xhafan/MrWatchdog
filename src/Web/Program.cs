17aea77c661a3eab95df3b401c517a2a2dae99f2
xhafan <xhafan@gmail.com>
2025-11-20 10:33:54 +0100

    #1 First version - temporarily disabling forwarded headers to see what Kamal proxy sends (it sends X-Forwarded-For but not sure about X-Forwarded-Port)

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -349,15 +349,15 @@ public class Program
 
         // Configure the HTTP request pipeline.
             
-        var forwardedHeadersOptions = new ForwardedHeadersOptions
-        {
-            ForwardedHeaders = ForwardedHeaders.XForwardedFor // identify client IP address
-                               | ForwardedHeaders.XForwardedProto // to make login via Google use https
-        };
-        forwardedHeadersOptions.KnownNetworks.Clear();
-        forwardedHeadersOptions.KnownProxies.Clear();
-        app.UseForwardedHeaders(forwardedHeadersOptions);
-        app.UseMiddleware<ForwardedPortMiddleware>();
+        // var forwardedHeadersOptions = new ForwardedHeadersOptions
+        // {
+        //     ForwardedHeaders = ForwardedHeaders.XForwardedFor // identify client IP address
+        //                        | ForwardedHeaders.XForwardedProto // to make login via Google use https
+        // };
+        // forwardedHeadersOptions.KnownNetworks.Clear();
+        // forwardedHeadersOptions.KnownProxies.Clear();
+        // app.UseForwardedHeaders(forwardedHeadersOptions);
+        // app.UseMiddleware<ForwardedPortMiddleware>();
 
         app.UseResponseCompression();
         

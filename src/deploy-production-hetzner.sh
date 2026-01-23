f482682b7d557a3404251d04da6a2951eb4c4ec1
xhafan <xhafan@gmail.com>
2026-01-23 11:00:27 +0100

    #1 First version - fixing getting staging version in production deployment scripts

--- src/deploy-production-hetzner.sh
+++ src/deploy-production-hetzner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eux
 
-STAGING_VERSION=$(kamal app version -d staging-local)
+STAGING_VERSION=$(kamal app version -d staging-local -q | tail -n 2 | head -n 1)
 
 # Deploy to production-hetzner (pull staging image + deploy it)
 kamal deploy -d production-hetzner -P --version=$STAGING_VERSION
--- src/deploy-production-local.sh
+++ src/deploy-production-local.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eux
 
-STAGING_VERSION=$(kamal app version -d staging-local)
+STAGING_VERSION=$(kamal app version -d staging-local -q | tail -n 2 | head -n 1)
 
 # Deploy to production-local (pull staging image + deploy it)
 kamal deploy -d production-local -P --version=$STAGING_VERSION

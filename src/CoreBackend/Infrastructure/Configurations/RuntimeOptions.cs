0bb49bef89e1ea2d57da269e1a28c219e6e6cfc8
xhafan <xhafan@gmail.com>
2026-04-07 15:42:55 +0200

    #1 First version - removing RuntimeOptions.Environment as it was not used; IHostEnvironment.EnvironmentName should be used instead

--- src/CoreBackend/Infrastructure/Configurations/RuntimeOptions.cs
+++ src/CoreBackend/Infrastructure/Configurations/RuntimeOptions.cs
@@ -3,6 +3,5 @@
 public class RuntimeOptions
 {
     public string Url { get; set; } = null!;
-    public string Environment { get; set; } = null!;
     public string? AppGitShaVersion { get; set; }
 }
